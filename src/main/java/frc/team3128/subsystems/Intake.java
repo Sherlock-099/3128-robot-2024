88d96f2508550ec491b94a4b23cd03947c2f5573
Tyler Enderwick <tyler.enderwick@gmail.com>
2024-10-25 17:16:57 -0700

    Update Intake.java

--- src/main/java/frc/team3128/subsystems/Intake.java
+++ src/main/java/frc/team3128/subsystems/Intake.java
@@ -6,6 +6,9 @@ import static edu.wpi.first.wpilibj2.command.Commands.runOnce;
 import static edu.wpi.first.wpilibj2.command.Commands.sequence;
 import static edu.wpi.first.wpilibj2.command.Commands.waitSeconds;
 import static edu.wpi.first.wpilibj2.command.Commands.waitUntil;
+
+import java.lang.Thread.State;
+import java.lang.management.MonitorInfo;
 import java.util.function.BooleanSupplier;
 import java.util.function.DoubleSupplier;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
@@ -25,6 +28,9 @@ public class Intake {
 
     public enum Setpoint {
         //define your enums here (the diff heights you want to place cones at - ie. low pole, mid pole, etc)
+        LOWPOLE(0),
+        MIDPOLE(45),
+        HIGHPOLE(70)
         ;
 
         public final double angle;
@@ -65,28 +71,38 @@ public class Intake {
     
     public class IntakeRollers extends ManipulatorTemplate { 
 
+    private DigitalInput limitSwitch;
+
     private IntakeRollers() {
         super(CURRENT_THRESHHOLD, INTAKE_POWER, OUTTAKE_POWER, STALL_POWER, 0.3, MOTOR);
+
         /*
         *define constants for all of the above parameters 
-           * Creates an Manipulator object.
-     *currentThreshold Current when object is intook.
-     *intakePower Intake power.
-     *outtakePower Outtake power.
-     *stallPower Stall Power, run when Manipulator has a game piece.
-     *lagSeconds Time before current check is run.
-     *motors Manipulator motors.
+        * Creates an Manipulator object.
+        *currentThreshold Current when object is intook.
+        
+        *intakePower Intake power.
+        *outtakePower Outtake power.
+        *stallPower Stall Power, run when Manipulator has a game piece.
+        *lagSeconds Time before current check is run.
+        *motors Manipulator motors.
         */
-
+        
+        //??????????????????????????????????????limitSwitch = new DigitalInput(7);
         initShuffleboard();
     }
 
     @Override
     protected void configMotors() {
-        //define one motor (again, using nar_canspark)
+        //NAR_CANSpark
+        //define one motor (again, using nar_canspark)???
+       
         //setInverted as false
+        MOTOR.setInverted(false);
         //setNeutralMode as COAST
+        MOTOR.setNeutralMode(Neutral.COAST);
         //setCurrentLimit as 40
+        MOTOR.setCurrentLimit(40);
         }
     
 
@@ -101,10 +117,18 @@ public class Intake {
 
     //create a getInstance() so you can always create a new intake 
     //ie. public static synchronized Intake getInstance(){
+    public static synchronized Intake getInstance() {
+        if(instance == null) {
+            instance = new Intake();
+        }
+        return instance;
+    }
 
     private Intake(){
         //create a new intakePivot 
+        intakePivot = new IntakePivot();
         //create a new intakeRollers
+        intakeRollers = new IntakeRollers();
     }
 
     
@@ -112,8 +136,10 @@ public class Intake {
         return sequence(
             deadline(
                 //use intakeRollers to call intake(). Remember to add a comma after your sentence (proper format for sequence/parallel commands)
+                intakeRollers.intake(),
                 sequence(
                     //have your intakePivot pivot to setpoint.angle
+                    intakePivot.pivotTo(setpoint.angle),
                     waitUntil(() -> hasObjectPresent()),
                     runOnce(() -> setPower(STALL_POWER))
                 )
@@ -124,11 +150,12 @@ public class Intake {
     public Command outtake() {
         return sequence (
             //outtaking so have your intakePivot pivot to your enum state
+            intakePivot.pivotTo(Setpoint.LOWPOLE.angle),
             //have intakeRollers run manipulator at outake power
-
+            intakeRollers.runManipulator(OUTTAKE_POWER)
         );
     }
-
+    
     public boolean hasObjectPresent(){
         return Math.abs(getCurrent()) > CURRENT_THRESHHOLD;
     }
